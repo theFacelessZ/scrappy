d3720bcb67d1caeb01113462b92f0f43847fc071
Anthony Lupow <anthony.lupow@gmail.com>
2020-09-20 18:29:29 +0300

    Implement a basic json source.

---
+++ src/Sources/JsonSource.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Scrappy\Sources;
+
+use GuzzleHttp\Client;
+use Scrappy\SourceBase;
+
+abstract class JsonSource extends SourceBase {
+
+    /**
+     * Http client.
+     *
+     * @var Client
+     */
+    protected $client;
+
+    /**
+     * A set of http client options.
+     *
+     * @var array
+     */
+    protected $options;
+
+    /**
+     * JsonSource constructor.
+     */
+    public function __construct()
+    {
+        $this->client = new Client();
+    }
+
+    /**
+     * Sets global request options.
+     *
+     * @param array $options
+     *   An array of client options.
+     *
+     * @return $this
+     */
+    public function setOptions(array $options) {
+        $this->options = $options;
+
+        return $this;
+    }
+
+    /**
+     * Performs a request.
+     *
+     * @param $method
+     *   Request method.
+     * @param $url
+     *   Request url.
+     * @param array $options
+     *   Request options.
+     *
+     * @return false|string
+     *   Json object.
+     *
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    protected function request($method, $url, array $options = []) {
+        $response = $this->client->request(
+            $method, $url, array_merge($this->options, $options)
+        );
+
+        return json_encode($response->getBody()->getContents());
+    }
+
+}
